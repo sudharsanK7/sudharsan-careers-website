0014271989b89fc2e3bf81fa1a6f6f0e4651b41a
sudharsanK7 <94968082+sudharsanK7@users.noreply.github.com>
2023-12-05 15:39:35 +0000

    Added bootstrap and made cleaner code using Flask and also created a jsonify page

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs 10,000,00'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Mumbai, India',
+    'salary': 'Rs 20,000,00'
+  },
+  {
+    'id': 3,
+    'title': 'Front-end Engineer',
+    'location': 'Chennai, India'
+  },
+  {
+    'id': 4,
+    'title': 'Full-stack Developer',
+    'location': 'San francisco, USA',
+    'salary': '$ 120,000'
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", 
+                         jobs=JOBS, 
+                         company_name = "Speed's")
+
+@app.route("/api/jobs")
+def job_list():
+  return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
